d62188f653d7551deb11c5598755268a0fe213c6
EricaReis <ericareis67@outlook.com>
2024-01-09 22:48:06 -0300

    Add AuthForm Page

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,99 @@
+"use client"
+
+import Button from "@/app/components/button"
+import Input from "@/app/components/inputs/input"
+import { useCallback, useState } from "react"
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form"
+
+type Variant = "LOGIN" | "REGISTER"
+
+export default function AuthForm() {
+  const [variant, setVariant] = useState<Variant>("LOGIN")
+  const [isLoading, setIsLoading] = useState(false)
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER")
+    } else {
+      setVariant("LOGIN")
+    }
+  }, [variant])
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  })
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true)
+
+    if (variant === "REGISTER") {
+      // Axios Register
+    }
+
+    if (variant === "LOGIN") {
+      // NextAuth SignIn
+    }
+  }
+
+  const socialAction = (action: string) => {
+    setIsLoading(true)
+
+    //NextAuth Social Sign In
+  }
+
+  return (
+    <div
+      className="
+        mt-8 
+        sm:mx-auto 
+        sm:w-full 
+        sm:max-w-md
+    "
+    >
+      <div
+        className="
+          bg-white 
+          px-4 
+          py-8 
+          shadow 
+          sm:rounded-lg 
+          sm:px-10
+      "
+      >
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGISTER" && (
+            <Input id="name" label="Name" register={register} errors={errors} />
+          )}
+
+          <Input
+            id="email"
+            label="E-mail Address"
+            type="email"
+            register={register}
+            errors={errors}
+          />
+
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+          />
+
+          <div>
+            <Button>Test</Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
