e12e27ae418380677974b02d2048e0bf9ef8866e
EricaReis <ericareis67@outlook.com>
2024-01-10 22:26:56 -0300

    Finish to implement Credentials Provider with NextAuth

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -8,6 +8,7 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form"
 import AuthSocialButton from "./AuthSocialButton"
 import { BsGithub, BsGoogle } from "react-icons/bs"
 import toast from "react-hot-toast"
+import { signIn } from "next-auth/react"
 
 type Variant = "LOGIN" | "REGISTER"
 
@@ -46,7 +47,17 @@ export default function AuthForm() {
     }
 
     if (variant === "LOGIN") {
-      // NextAuth SignIn
+      signIn("credentials", { ...data, redirect: false })
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error("Invalid credentials")
+          }
+
+          if (callback?.ok && !callback?.error) {
+            toast.success("Logged in!")
+          }
+        })
+        .finally(() => setIsLoading(false))
     }
   }
 
