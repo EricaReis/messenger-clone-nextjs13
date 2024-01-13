e098b2c61a163f86ba09e1d1ab1af7109516b605
EricaReis <ericareis67@outlook.com>
2024-01-12 21:25:02 -0300

    Add mobile footer view

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from "react"
 import DesktopSidebar from "./DesktopSidebar"
+import MobileFooter from "./MobileFooter"
 
 export default async function Sidebar({ children }: { children: ReactNode }) {
   return (
     <div className="h-full">
       <DesktopSidebar />
-      {/* <MobileFooter /> */}
+      <MobileFooter />
       <main className="lg:pl-20 h-full">{children}</main>
     </div>
   )
