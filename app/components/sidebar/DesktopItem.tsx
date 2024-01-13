ca5cc2137cd6246e2a5cff14588bac94ea7712c5
EricaReis <ericareis67@outlook.com>
2024-01-12 21:07:53 -0300

    Add DesktopItem styles

--- app/components/sidebar/DesktopItem.tsx
+++ app/components/sidebar/DesktopItem.tsx
@@ -13,7 +13,7 @@ interface DesktopItemProps {
 
 export default function DesktopItem({
   label,
-  icon,
+  icon: Icon,
   href,
   onClick,
   active,
@@ -24,5 +24,30 @@ export default function DesktopItem({
     }
   }
 
-  return <div>DesktopItem</div>
+  return (
+    <li onClick={handleClick}>
+      <Link
+        className={clsx(
+          `
+          group
+          flex
+          gap-x-3
+          rounded-md
+          p-3
+          text-sm
+          leading-6
+          font-semibold
+          text-gray-500
+          hover:text-black
+          hover:bg-gray-100
+        `,
+          active && "bg-gray-100 text-black"
+        )}
+        href={href}
+      >
+        <Icon className="h-6 w-6 shrink-0" />
+        <span className="sr-only">{label}</span>
+      </Link>
+    </li>
+  )
 }
--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -5,6 +5,7 @@ export default async function Sidebar({ children }: { children: ReactNode }) {
   return (
     <div className="h-full">
       <DesktopSidebar />
+      {/* <MobileFooter /> */}
       <main className="lg:pl-20 h-full">{children}</main>
     </div>
   )
