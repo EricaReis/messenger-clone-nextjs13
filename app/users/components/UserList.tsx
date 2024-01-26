2878ee2be081c4193e83b4404f46c6e079fe28a9
EricaReis <ericareis67@outlook.com>
2024-01-25 21:49:27 -0300

    Finish people's page

--- app/users/components/UserList.tsx
+++ app/users/components/UserList.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { User } from "@prisma/client"
+
 import UserBox from "./UserBox"
 
 interface UserListProps {
@@ -11,35 +12,32 @@ const UserList: React.FC<UserListProps> = ({ items }) => {
   return (
     <aside
       className="
-        fixed
-        inset-y-0
+        fixed 
+        inset-y-0 
         pb-20
         lg:pb-0
-        lg:left-20
-        lg:2-80
+        lg:left-20 
+        lg:w-80 
         lg:block
-        overflow-y-auto
-        border-r
+        overflow-y-auto 
+        border-r 
         border-gray-200
-        block
-        w-full
-        left-0
+        block w-full left-0
       "
     >
       <div className="px-5">
         <div className="flex-col">
           <div
             className="
-              text-2xl
-              font-bold
-              text-neutral-800
+              text-2xl 
+              font-bold 
+              text-neutral-800 
               py-4
             "
           >
             People
           </div>
         </div>
-
         {items.map((item) => (
           <UserBox key={item.id} data={item} />
         ))}
