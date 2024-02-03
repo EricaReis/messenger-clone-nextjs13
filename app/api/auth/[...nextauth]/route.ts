20d106a360225558adad7591485fc8d6d3415c78
EricaReis <ericareis67@outlook.com>
2024-02-03 17:14:38 -0300

    Move authOptions to his own folder

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,9 @@
+import { authOptions } from "@/app/libs/authOptions";
+import NextAuth from "next-auth";
+
+
+
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- app/libs/authOptions.ts
+++ app/libs/authOptions.ts
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt"
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
@@ -7,7 +7,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter"
 
 import prisma from "@/app/libs/prismadb"
 
-export const authOptions: NextAuthOptions = {
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
@@ -58,7 +58,3 @@ export const authOptions: NextAuthOptions = {
   },
   secret: process.env.NEXTAUTH_SECRET,
 }
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
