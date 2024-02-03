e66c46fc7d739e41729ef38aa8f571efcad970ee
EricaReis <ericareis67@outlook.com>
2024-02-03 16:07:34 -0300

    Add type to nextauth options

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -59,6 +59,6 @@ export const authOptions: AuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
 }
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions) as never;
 
 export { handler as GET, handler as POST };
